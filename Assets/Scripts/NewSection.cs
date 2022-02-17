58083f09c0ce122278d16aa6bf5503ed26aae1d9
EricTran000 <89489669+EricTran000@users.noreply.github.com>
2022-02-17 16:57:11 -0600

    Created a script that loads the next level

---
+++ Assets/Scripts/NewSection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewSection : MonoBehaviour
+{
+    bool newSection;
+
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            newSection = true;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Loads the next level if the player touches the gameobject
+    void Update()
+    {
+       if(newSection)
+        {
+            SceneManager.LoadScene("second level");
+        }
+    }
+}
